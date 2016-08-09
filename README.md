0b14f4fd2be9d5590db090463816c6b6b44b6ac4
David K. Bainbridge <dbainbri@ciena.com>
2016-08-09 07:41:28 -0700

    add comments to module code, add the query state and update documentation accordingly.

--- README.md
+++ README.md
@@ -5,9 +5,17 @@ This is an ansible module that can be used to manage the definitions in the linu
 # How It Works
 This module first parses the `/etc/network/interfaces` file and the evaluates the
 ansible task specification to what exists in the file. When thre is a change the
-module writes the updated file back to storage and return a changed status of `True`.
+module writes the updated file back to storage and return a changed status of `True.
 If there is no change then the file is not written unless the `force` option is
 set to True.
+
+In addition to the states `present` and `absent`, the `state` attribute can also
+be set to `query` to query the value of a given interface. If `query` is specified
+then if the named interface exists the result of the task will be unchanged and the
+result will contain the attribute `found` set to `true` and the attribtue `interface`
+set to the value of the interface. If the named interface does not exist then the
+result will be unchanged and the result wil container the attribute `found` set to
+`false`.
 
 If the file is written and comments in the existing file will be lost as comment
 information is not maintained in the model of the file's information.
@@ -18,7 +26,7 @@ every possible option. Specific attributes of interest are:
 
 - **src** - the source interface file, defaults to `/etc/network/interfaces`
 - **dest** - the file to write if required, default to src if not specified
-- **state** - present if the specification must be in the file, absent if it shouldn't
+- **state** - `present` if the specification must be in the file, `absent` if it shouldn't. Additionally the state can be `query` to query an interface specification from the file.
 - **name** - name of the interface
 - **auto** - should the interface be auto enabled
 - **type** - type of the interface, defaults to `inet`
--- netfile.py
+++ netfile.py
@@ -8,9 +8,20 @@ import shlex
 import string
 import ipaddress
 
+# Regular expressions to identify comments and blank lines
 comment = re.compile("^\s*#")
 blank = re.compile("^\s*$")
 
+#####
+# Parsers
+#
+# Parses are methods that take the form 'parse_<keyword>', where the keyword
+# is the first word on a line in file. The purpose of the parser is to
+# evaluate the line tna update the interface model accordingly.
+####
+
+# Creates an interface definition in the model and sets the auto
+# configuration to true
 def parse_auto(data, current, words):
 	if words[1] in data.keys():
 		iface = data[words[1]]
@@ -21,6 +32,8 @@ def parse_auto(data, current, words):
 	data[words[1]] = iface
 	return words[1]
 
+# Creates an interface definition in the model if one does not exist and
+# sets the type and configuation method
 def parse_iface(data, current, words):
 	if words[1] in data.keys():
 		iface = data[words[1]]
@@ -32,6 +45,9 @@ def parse_iface(data, current, words):
 	data[words[1]] = iface
 	return words[1]
 
+# Evaluates the address specification and either sets the address only or if the
+# address is specified with network bits (mask) sets the network and netmask as
+# well.
 def parse_address(data, current, words):
 	if current == "":
                 raise SyntaxError("Attempt to add attribute '%s' without an interface" % words[0])
@@ -53,6 +69,8 @@ def parse_address(data, current, words):
 	data[current] = iface
 	return current
 
+# Used to evaluate attributes and add a generic name / value pair to the interface
+# model
 def parse_add_attr(data, current, words):
 	if current == "":
 		raise SyntaxError("Attempt to add attribute '%s' without an interface" % words[0])
@@ -66,9 +84,19 @@ def parse_add_attr(data, current, words):
         data[current] = iface
 	return current
 
+#####
+# Writers
+#
+# Writers take the form of 'write_<keyword>` where keyword is an interface
+# attribute. The role of the writer is to output the attribute to the
+# output stream, i.e. the new interface file.
+#####
+
+# Writes a generic name / value pair indented
 def write_attr(out, name, value):
 	out.write("  %s %s\n" % (name, value))
 
+# Writes an interface definition to the output stream
 def write_iface(out, name, iface):
 	if "auto" in iface.keys() and iface["auto"] == "True":
 		out.write("auto %s\n" % (name))
@@ -83,6 +111,7 @@ def write_iface(out, name, iface):
 			write_attr(out, attr, iface[attr])
 	out.write("\n")
 
+# Writes the new interface file
 def write(out, data):
 	out.write("# This file describes the network interfaces available on your system\n")
 	out.write("# and how to activate them. For more information, see interfaces(5).\n\n")
@@ -97,6 +126,7 @@ def write(out, data):
 			continue
 		write_iface(out, iface, data[iface])
 
+# The defaults for the netfile task
 src_file = "/etc/network/interfaces"
 dest_file = ""
 state = "present"
@@ -106,10 +136,12 @@ values = {
 	"config": "manual",
 	"type": "inet"
 }
+
 # read the argument string from the arguments file
 args_file = sys.argv[1]
 args_data = file(args_file).read()
 
+# parse the task options
 arguments = shlex.split(args_data)
 for arg in arguments:
 	# ignore any arguments without an equals in it
@@ -131,8 +163,16 @@ for arg in arguments:
 	elif key[0] != '_':
 		values[key] = value
 
+# all methods is used to check if parser or writer methods exist
 all_methods = dir()
+
+# which attributes should be ignored and not be written as single
+# attributes values against and interface
 write_ignore = ["auto", "type", "config"]
+
+# specifies the order in which attributes are written against an
+# interface. Any attribute note in this list is sorted by default
+# order after the attributes specified.
 write_sort_order = {
 		"address" :   1,
 		"network" :   2,
@@ -141,13 +181,18 @@ write_sort_order = {
 		"gateway" :   5
 }
 
+# Read and parse the specified interface file
 file = open(src_file, "r")
 ifaces = {}
-current = ""
+current = "" # The current interface being parsed
 for line in file.readlines():
 	line = line.rstrip('\n')
+
+	# Drop any comment of blank line
 	if comment.match(line) or blank.match(line):
 		continue
+
+	# Parse the line
 	words = line.split()
 	parser = "parse_" + words[0].replace("-", "_")
 	if parser in all_methods:
@@ -157,10 +202,20 @@ for line in file.readlines():
 
 file.close()
 
+# Assume no change unless we discover otherwise
 result = {
 	"changed" : False
 }
-if state == "present":
+
+# if the interface specified and state is present then either add
+# it to the model or replace it if it already exists.
+if state == "query":
+	if name in ifaces.keys():
+		result["interface"] = ifaces[name]
+		result["found"] = True
+	else:
+		result["found"] = False
+elif state == "present":
 	if name in ifaces.keys():
 		have = ifaces[name]
 		if cmp(have, values) != 0:
@@ -171,13 +226,17 @@ if state == "present":
 		result["changed"] = True
 
 
+# if state is absent then remove it from the model
 elif state == "absent" and name in ifaces.keys():
 	del ifaces[name]
 	result["changed"] = True
 
+# Only write the output file if something has changed or if the
+# task requests a forced write.
 if force or result["changed"]:
 	file = open(dest_file, "w+")
 	write(file, ifaces)
 	file.close()
 
+# Output the task result
 print json.dumps(result)
